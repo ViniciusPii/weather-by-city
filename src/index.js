9290ef177c38c640639f70a6852abe43fc63df44
Vinicius Pii <viniciuspii@MacBook-Pro-de-Vinicius.local>
2019-07-19 14:09:55 -0300

    comunicação com a api de front

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const app = express()
 
 const port = 5000
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(weatherController)
 
 app.listen(port,() => {
